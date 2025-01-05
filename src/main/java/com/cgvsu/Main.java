20243d0cb25438d3466ccb1f8835d8144768d314
MrHouston <mrhoustonvr@yandex.ru>
2025-01-05 16:01:06 +0300

    Тестовая ветка тестим ветку

--- src/main/java/com/cgvsu/Main.java
+++ src/main/java/com/cgvsu/Main.java
@@ -3,5 +3,6 @@ package com.cgvsu;
 public class Main {
     public static void main(String[] args) {
         Simple3DViewer.main(args);
+        System.out.println("test of test nigga!");
     }
 }
