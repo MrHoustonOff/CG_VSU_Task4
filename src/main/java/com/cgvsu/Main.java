673adfbe7ab4a2b471f5d54f1ae782a429fdd3c9
MrHouston <mrhoustonvr@yandex.ru>
2025-01-05 19:25:27 +0300

    Добавил библиотеку Math за авторством Василия Мельника ❤️

--- src/main/java/com/cgvsu/Main.java
+++ src/main/java/com/cgvsu/Main.java
@@ -3,6 +3,5 @@ package com.cgvsu;
 public class Main {
     public static void main(String[] args) {
         Simple3DViewer.main(args);
-        System.out.println("test of test nigga!");
     }
 }
